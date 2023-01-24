a741975ab90dd01c687521225f47922e689d8b22
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-24 14:25:36 +0000

    Add counter and input tracking challenges

--- app/routes/counter/index.tsx
+++ app/routes/counter/index.tsx
@@ -1,9 +1,37 @@
+import Navbar from "~/components/Navbar";
+import Paragraph from "~/components/Paragraph";
+
 interface CounterProps {}
 
 const Counter = ({}: CounterProps) => {
   return (
     <>
-      <h1 className="text-4xl text-blue-500">Counter </h1>
+      <Navbar />
+      <main className="container mx-auto flex min-h-screen items-center justify-center">
+        <div className="m-8 rounded-xl bg-sky-100 p-20 shadow-xl">
+          <h1 className="mb-8 text-center text-6xl font-bold text-blue-500">
+            Counter
+          </h1>
+          <div className="text-center">
+            <Paragraph>
+              Can you update the component code so that clicking the 'Add'
+              button below makes the number shown increment by one every time?
+            </Paragraph>
+          </div>
+          <div className="flex items-center justify-center p-10">
+            <div>
+              <div className="flex h-64 w-64 items-center justify-center rounded-full bg-white">
+                <h2 className="text-6xl text-indigo-700">1</h2>
+              </div>
+              <div className="mt-10 flex justify-center">
+                <button className="rounded-lg bg-indigo-700 px-12 py-6 text-2xl text-white">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
--- app/routes/todo-app/index.tsx
+++ app/routes/todo-app/index.tsx
@@ -1,49 +1,53 @@
 import ExternalLink from "~/components/ExternalLink";
 import ListItem from "~/components/ListItem";
+import Navbar from "~/components/Navbar";
 import Paragraph from "~/components/Paragraph";
 
 const TodoApp = () => {
   return (
-    <main className="mx-auto flex min-h-screen items-center justify-center">
-      <div className="m-4 w-256 rounded-xl bg-sky-100 p-20 shadow-xl">
-        <h1 className="mb-8 text-center text-6xl font-bold text-blue-500">
-          ToDo App
-        </h1>
-        <Paragraph>
-          Your task is to replace this content (found in{" "}
-          <code className="text-blue-600">app/routes/todo-app/index.tsx</code>)
-          with code that will build a ToDo app. It should allow the user to:
-        </Paragraph>
-        <ol className="my-4 ml-16">
-          <ListItem>Create a ToDo</ListItem>
-          <ListItem>Delete ToDos</ListItem>
-          <ListItem>OPTIONAL EXTRA CREDIT: Edit ToDos</ListItem>
-        </ol>
-        <Paragraph>
-          The GIF below shows an example of how the finished product{" "}
-          <em>might</em> look and behave, but feel free to put your own spin on
-          the solution.
-        </Paragraph>
-        <div className="flex justify-center">
-          <img
-            className="max-w-96 mb-4 rounded-lg"
-            src="https://res.cloudinary.com/dptdx8zuv/image/upload/v1674474087/react-introduction/2023-01-23_11-37-47_1_n7lt3y.gif"
-            alt="Finished ToDo app example"
-          />
+    <>
+      <Navbar />
+      <main className="mx-auto flex min-h-screen items-center justify-center">
+        <div className="m-4 w-256 rounded-xl bg-sky-100 p-20 shadow-xl">
+          <h1 className="mb-8 text-center text-6xl font-bold text-blue-500">
+            ToDo App
+          </h1>
+          <Paragraph>
+            Your task is to replace this content (found in{" "}
+            <code className="text-blue-600">app/routes/todo-app/index.tsx</code>
+            ) with code that will build a ToDo app. It should allow the user to:
+          </Paragraph>
+          <ol className="my-4 ml-16">
+            <ListItem>Create a ToDo</ListItem>
+            <ListItem>Delete ToDos</ListItem>
+            <ListItem>OPTIONAL EXTRA CREDIT: Edit ToDos</ListItem>
+          </ol>
+          <Paragraph>
+            The GIF below shows an example of how the finished product{" "}
+            <em>might</em> look and behave, but feel free to put your own spin
+            on the solution.
+          </Paragraph>
+          <div className="flex justify-center">
+            <img
+              className="max-w-96 mb-4 rounded-lg"
+              src="https://res.cloudinary.com/dptdx8zuv/image/upload/v1674474087/react-introduction/2023-01-23_11-37-47_1_n7lt3y.gif"
+              alt="Finished ToDo app example"
+            />
+          </div>
+          <Paragraph>
+            You should also ensure the app is styled professionally using
+            Tailwind CSS.
+          </Paragraph>
+          <Paragraph>
+            Be sure to use an online Tailwind CSS cheatsheet like{" "}
+            <ExternalLink to="https://nerdcave.com/tailwind-cheat-sheet">
+              this one
+            </ExternalLink>{" "}
+            to help you.
+          </Paragraph>
         </div>
-        <Paragraph>
-          You should also ensure the app is styled professionally using Tailwind
-          CSS.
-        </Paragraph>
-        <Paragraph>
-          Be sure to use an online Tailwind CSS cheatsheet like{" "}
-          <ExternalLink to="https://nerdcave.com/tailwind-cheat-sheet">
-            this one
-          </ExternalLink>{" "}
-          to help you.
-        </Paragraph>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
