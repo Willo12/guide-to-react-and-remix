de41a931e8b7ad4dcabae119318326e92793a403
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-24 10:23:06 +0000

    Fix link to admin

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -21,7 +21,7 @@ export default function PostSlug() {
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
-      <Link to="admin" className="text-red-600 underline">
+      <Link to="/posts/admin" className="text-red-600 underline">
         Admin
       </Link>
       <article className="prose">
