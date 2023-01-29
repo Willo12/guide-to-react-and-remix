0532c4c2cf16a7dae101db2d666d8b986eb4e954
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-29 15:34:55 +0000

    refactor: use MonoType in Counter instructions

--- app/routes/counter/index.tsx
+++ app/routes/counter/index.tsx
@@ -1,4 +1,5 @@
 import CounterChallengeCode from "~/challenge-components/react/counter-challenge-code";
+import MonoType from "~/components/MonoType";
 import Paragraph from "~/components/Paragraph";
 
 const Counter = () => {
@@ -8,12 +9,7 @@ const Counter = () => {
         <h1 className="mb-8 text-center text-6xl font-bold text-blue-500">
           Counter
         </h1>
-        <div className="mb-6 flex items-center justify-center">
-          <span className="pr-3 text-slate-700">Location:</span>
-          <code className="rounded bg-slate-900 py-2 px-3 font-mono text-white">
-            app/challenge-components/counter-challenge-code.tsx
-          </code>
-        </div>
+        <MonoType path="app/challenge-components/counter-challenge-code.tsx" />
         <Paragraph>
           Can you update the component code at the location above so that
           clicking the 'Add' button makes the number increment by one every
