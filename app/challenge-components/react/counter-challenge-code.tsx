c88d004df6523b6ab91d043c172428cb309ea84b
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-29 14:25:01 +0000

    feat: update counter challenge layout

---
+++ app/challenge-components/react/counter-challenge-code.tsx
@@ -0,0 +1,24 @@
+const CounterChallengeCode = () => {
+  // 🤓 CHALLENGE: Can you update this component code so that clicking the 'Add' button increments the number by one?
+  // 🚀 EXTRA CREDIT 1: Can you add a second button that, when clicks, decrements the number by one?
+  // 🚀 EXTRA CREDIT 2: Can you ensure that the number is never decremented below 0?
+
+  return (
+    <div className="flex items-center justify-center p-10">
+      <div>
+        <div className="flex h-64 w-64 items-center justify-center rounded-full bg-white">
+          <h2 className="flex h-36 w-36 items-center justify-center rounded-full bg-yellow-200 text-6xl text-blue-500">
+            1
+          </h2>
+        </div>
+        <div className="mt-10 flex justify-center">
+          <button className="rounded-lg bg-indigo-700 px-12 py-6 text-2xl text-white">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CounterChallengeCode;
--- app/routes/counter/index.tsx
+++ app/routes/counter/index.tsx
@@ -1,32 +1,27 @@
+import CounterChallengeCode from "~/challenge-components/react/counter-challenge-code";
 import Paragraph from "~/components/Paragraph";
 
-interface CounterProps {}
-
-const Counter = ({}: CounterProps) => {
+const Counter = () => {
   return (
-    <main className="container mx-auto flex min-h-screen items-center justify-center">
-      <div className="m-8 rounded-xl bg-sky-100 p-20 shadow-xl">
+    <main className="container mx-auto min-h-screen items-center justify-center lg:flex">
+      <div className="m-8 rounded-xl bg-sky-100 p-20 shadow-xl lg:w-1/2">
         <h1 className="mb-8 text-center text-6xl font-bold text-blue-500">
           Counter
         </h1>
-        <div className="text-center">
-          <Paragraph>
-            Can you update the component code so that clicking the 'Add' button
-            below makes the number shown increment by one every time?
-          </Paragraph>
+        <div className="mb-6 flex items-center justify-center">
+          <span className="pr-3 text-slate-700">Location:</span>
+          <code className="rounded bg-slate-900 py-2 px-3 font-mono text-white">
+            app/challenge-components/counter-challenge-code.tsx
+          </code>
         </div>
-        <div className="flex items-center justify-center p-10">
-          <div>
-            <div className="flex h-64 w-64 items-center justify-center rounded-full bg-white">
-              <h2 className="text-6xl text-indigo-700">1</h2>
-            </div>
-            <div className="mt-10 flex justify-center">
-              <button className="rounded-lg bg-indigo-700 px-12 py-6 text-2xl text-white">
-                Add
-              </button>
-            </div>
-          </div>
-        </div>
+        <Paragraph>
+          Can you update the component code at the location above so that
+          clicking the 'Add' button makes the number increment by one every
+          time?
+        </Paragraph>
+      </div>
+      <div className="lg:w-1/2">
+        <CounterChallengeCode />
       </div>
     </main>
   );
