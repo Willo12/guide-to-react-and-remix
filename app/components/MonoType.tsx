ca2874b8dd40ae80916d9a93cd0a0d9fca185129
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-31 18:20:15 +0000

    style: force long string line breaks in MonoType

--- app/components/MonoType.tsx
+++ app/components/MonoType.tsx
@@ -7,7 +7,7 @@ const MonoType = ({ path, label }: MonoTypeProps) => {
   return (
     <div className="mb-6 flex items-center justify-center">
       {label ? <span className="pr-3 text-slate-700">{label}</span> : null}
-      <code className="rounded bg-slate-900 py-2 px-3 font-mono text-white">
+      <code className="break-all rounded bg-slate-900 py-2 px-3 font-mono text-white">
         {path}
       </code>
     </div>
