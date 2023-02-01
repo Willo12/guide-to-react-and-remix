aea0111315da4b449d05656168560dced0ae0f6f
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-01 09:01:46 +0000

    style: update styling challenge page layout

--- app/routes/styling/index.tsx
+++ app/routes/styling/index.tsx
@@ -1,12 +1,11 @@
+import stylingFinished from "~/assets/styling-pass.png";
 import StylingChallengeCode from "~/challengeComponents/react/StylingChallengeCode";
-import HandleUserInputChallengeCode from "~/challengeComponents/react/StylingChallengeCode";
 import ExternalLink from "~/components/ExternalLink";
 import { ExtraCreditLink } from "~/components/ExtraCreditLink";
 import HintBox from "~/components/HintBox";
 import ListItem from "~/components/ListItem";
 import MonoType from "~/components/MonoType";
 import Paragraph from "~/components/Paragraph";
-import stylingFinished from "~/assets/styling-pass.png";
 
 const Styling = () => {
   return (
@@ -19,19 +18,28 @@ const Styling = () => {
           <MonoType path="app/challengeComponents/react/stylingChallengeCode.tsx" />
           <Paragraph>
             Update the code in the file above so that the words 'First',
-            'Second', 'Third' and 'Fourth' (see below) are each placed into
+            'Second', 'Third' and 'Fourth', shown opposite, are each placed into
             boxes with the following styles:
-            <ul className="my-8">
-              <ListItem>Each box should be a light shade of green</ListItem>
-              <ListItem>
-                Every box should have a dark green border and dark green text
-              </ListItem>
-              <ListItem>Every box should be wider than the last</ListItem>
-              <ListItem>Every box should have a height of 64px</ListItem>
-              <ListItem>
-                There should be a gap (margin) of 8px around every box
-              </ListItem>
-            </ul>
+            <div className="flex">
+              <ul className="my-8">
+                <ListItem>Each box should be a light shade of green</ListItem>
+                <ListItem>
+                  Every box should have a dark green border and dark green text
+                </ListItem>
+                <ListItem>Every box should be wider than the last</ListItem>
+                <ListItem>Every box should have a height of 64px</ListItem>
+                <ListItem>
+                  There should be a gap (margin) of 8px around every box
+                </ListItem>
+              </ul>
+              <div className="flex w-1/2 items-center justify-center">
+                <img
+                  className="h-64 w-auto border-4 border-gray-300"
+                  src={stylingFinished}
+                  alt="Finished Challenge"
+                />
+              </div>
+            </div>
             You should only use Tailwind classes to acheive this.
           </Paragraph>
           <div className="mt-4">
@@ -58,12 +66,11 @@ const Styling = () => {
             <ExtraCreditLink to="/instructions/styling" />
           </div>
         </div>
-        <div className="flex w-1/2 items-center justify-center">
-          <img src={stylingFinished} alt="Finished Challenge" />
+        <div className="flex w-full items-center justify-center p-10">
+          <div className="border-4 border-gray-300 bg-white p-10">
+            <StylingChallengeCode />
+          </div>
         </div>
-      </div>
-      <div className="p-10">
-        <StylingChallengeCode />
       </div>
     </main>
   );
