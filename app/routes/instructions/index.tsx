b7018e300a2534750717abc98ce38a83e2132a64
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-02 10:37:28 +0000

    feat: implement instructions nested routes

--- app/routes/instructions/index.tsx
+++ app/routes/instructions/index.tsx
@@ -1,34 +1,7 @@
 import { Link, Outlet } from "@remix-run/react";
 
 const Instructions = () => {
-  return (
-    <div>
-      <h1 className="my-16 text-center text-2xl text-blue-500">Instructions</h1>
-      <div className="flex items-center justify-center">
-        <Link
-          className="m-4 bg-blue-400 p-8 uppercase tracking-wide text-white hover:bg-blue-600"
-          to="counter"
-        >
-          Counter
-        </Link>
-        <Link
-          className="m-4 bg-blue-400 p-8 uppercase tracking-wide text-white hover:bg-blue-600"
-          to="handle-user-input"
-        >
-          Handle User Input
-        </Link>
-        <Link
-          className="m-4 bg-blue-400 p-8 uppercase tracking-wide text-white hover:bg-blue-600"
-          to="todo-app"
-        >
-          Todo App
-        </Link>
-      </div>
-      <div>
-        <Outlet />
-      </div>
-    </div>
-  );
+  return <Link to="extra-credit">Extra Credit</Link>;
 };
 
 export default Instructions;
