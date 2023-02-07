38cda9c907216bcb070c8e212c094c661259dba5
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-07 19:21:44 +0000

    feat: add code snippet strings

---
+++ app/assets/codeSnippets/addingAModel/seedingDataSnippets.tsx
@@ -0,0 +1,64 @@
+export const createUserSnippet = `import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  const email = "rachel@remix.run";
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  // [...]
+}
+`;
+
+export const creatingUserAttributesSnippet = `async function seed() {
+  const email = "rachel@remix.run";
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  // [...]
+}`;
+
+export const prismaCreateSnippet = `const user = await prisma.user.create({
+  data: {
+    email,
+    password: {
+      create: {
+        hash: hashedPassword,
+      },
+    },
+  },
+});`;
