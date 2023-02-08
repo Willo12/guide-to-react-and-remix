fe4eb67ca7cf81507bd6340a2016bcb45e1cc46d
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-02-08 22:38:20 +0000

    style: fix navbar link hover

--- app/components/Navbar/NavbarLink.tsx
+++ app/components/Navbar/NavbarLink.tsx
@@ -9,7 +9,7 @@ const NavbarLink = ({ children, to }: NavbarLinkProps) => {
   return (
     <Link
       to={to}
-      className="mt-1 flex items-center rounded px-2 py-1 font-semibold text-white hover:bg-slate-800 md:mt-0 md:ml-2 md:first-of-type:ml-0"
+      className="mt-1 flex items-center rounded px-2 py-1 font-semibold text-white hover:bg-neutral-900 md:mt-0 md:ml-2 md:first-of-type:ml-0"
     >
       {children}
     </Link>
